0fee7966b2da2a4646a9356d47dc062073f822dc
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-23 12:26:07 -0400

    config. do header

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,6 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'import/prefer-default-export': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/control-has-associated-label': 0,
   },
 };
