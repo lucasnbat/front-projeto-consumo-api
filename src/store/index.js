04bf2fcfc70c1b8628b5d2e80de465822fe1ca8c
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-24 10:26:52 -0400

    mais sobre config de redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 // createStore recebe o reducer
 // reducer escuta a ação feita, cria novo estado que copia o estado atual
@@ -7,7 +8,15 @@ import { createStore } from 'redux';
 // state é o estado inicial
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const store = createStore(rootReducer);
+// criando o middleware saga
+const sagaMiddleware = createSagaMiddleware();
+
+// criando o store, passando reducer e middleware saga
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+// linha adicional para rodar o middleware saga
+sagaMiddleware.run(rootSaga);
 
 export default store;
