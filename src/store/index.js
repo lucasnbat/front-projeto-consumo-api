c20e21268b0c1df57d585e39784a70aaf4c504c0
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-23 20:52:35 -0400

    reducers, reducers, reducers

--- src/store/index.js
+++ src/store/index.js
@@ -6,31 +6,7 @@ import { createStore } from 'redux';
 // retorna o novo estado
 // state é o estado inicial
 
-/**
- * A lógica envolve:
- * 1. arquivo index do Store contendo o reducer com o switch
- * 2. Esse arquivo é acionado por qualquer outro arquivo que use o dispatch()
- * 3. useSelector() é usado para pegar o estado atual. Ele foi usado para mostrar
- *    as alternancias entre true e false ao apertar o botao
- */
-
-// estado inicial
-const initialState = {
-  botaoClicado: false,
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'BOTAO_CLICADO': {
-      const newState = { ...state }; // copia o estado atual
-      newState.botaoClicado = !newState.botaoClicado; // modifica o novo estado para o inverso
-      return newState;
-    }
-
-    default:
-      return state;
-  }
-};
+import reducer from './modules/example/reducer';
 
 const store = createStore(reducer);
 
